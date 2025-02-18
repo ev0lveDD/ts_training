2ce6c92b58bc107ed512a07dc6631b1a45a1f573
Jagiela <kjagiela93@gmail.com>
2025-02-18 06:43:35 +0100

    Enums, arrays, object from TypeScript

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,32 @@ export default function Home() {
     const result = num1 + num2;
     if(showResult) {
       console.log(resultPhrase + result);
-    } 
+      console.log(person.name);
+
+      for (const hobby of person.hobbies) {
+        console.log(hobby.toUpperCase());
+      }
+
+      if (person.role === Role.ADMIN) {
+        console.log('Is Admin!')
+      }
+     } 
      return resultPhrase + result;
   }
-  const [num1, setNum1] = useState<number | undefined>();
-  const [num2, setNum2] = useState<number | undefined>();
+  const [num1, setNum1] = useState<number>(0);
+  const [num2, setNum2] = useState<number>(0);
   const printResult = true;
   const phrase = "The Result is: ";
+
+  
+  enum Role { ADMIN, READ_ONLY, AUTHOR }
+
+  const person = {
+    name: 'Thorfinn',
+    age: 30,
+    hobbies: ['Sports', 'Cooking', 'Web-Dev'],
+    role: Role.ADMIN
+  }
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
