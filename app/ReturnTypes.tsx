7c342966cb8547c2867e59106d15e1fa619a1e04
Jagiela <kjagiela93@gmail.com>
2025-02-23 14:37:06 +0100

    js reminder with TS

--- app/ReturnTypes.tsx
+++ app/ReturnTypes.tsx
@@ -20,6 +20,35 @@ function add(num1: number, num2: number) {
     const [num1, setNum1] = useState<number>(0);
     const [num2, setNum2] = useState<number>(0);
 
+    const hobbies =['Sports', 'Cooking'];
+    const activeHobbies = ['Hiking'];
+
+    activeHobbies.push(...hobbies); // zamiast push można napisać -> const activeHobbies = ['Hiking', ...hobbies]
+    console.log(activeHobbies);
+
+    const person = {
+        firstName: 'Max',
+        age: 30
+    };
+
+    const copiedPerson = {...person};
+    console.log(copiedPerson);
+
+    const addRest = (...numbers: number[]) => {
+        return numbers.reduce((curResult, curValue) => {
+            return curResult + curValue;
+        }, 0);
+    };
+
+    const addedNumbers = addRest(5, 10, 2, 3.7);
+    console.log(addedNumbers);
+
+    const [hobby1, hobby2, ...remainingHobbies] = hobbies; // destrukturyzacja tablicy
+    console.log(hobbies, hobby1, hobby2);
+
+    const { firstName, age } = person; // destrukturyzacja obiektu
+    console.log(firstName, age, person);
+
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <input className="text-black" name="First value" onChange={e=>setNum1(+e.currentTarget.value)! as HTMLInputElement}></input>
